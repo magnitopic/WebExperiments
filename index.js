5a11ee95673d85e34f6d1eb0caf835f0148a1f5f
magnitopic <magnitopic@gmail.com>
2021-01-26 21:54:08 +0100

    /dice has a form to send data

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express= require('express');
 const mongoose= require('mongoose');
+const path=require('path');
 const Dice =require('./models/dice');
 
 const app=express();
 const dbURL='mongodb://localhost/NodeServerDB';
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(dbURL,{ useNewUrlParser: true , useUnifiedTopology: true })
     .then(result => app.listen(8080, () => console.log('\nServer runing on port 8080\n\nPress ctrl+c to stop')), console.log('Connection to DB successful'))
     .catch(err => console.log(err));
+
+app.get('/dice', (req,res)=>{
+    res.sendFile(path.join(__dirname+'/web/dice.html'));
+});
+
+app.post('/dice', (req, res) =>{
+    console.log(req.body)
+    //const dice = new Dice(req.body)
+});
 
 app.get('/add-dice', (req, res) => {
     const dice = new Dice({
@@ -24,5 +35,15 @@ app.get('/add-dice', (req, res) => {
             console.log(err);
         });
 });
+
+app.get('/all-dice', (req, res) =>{
+    Dice.find()
+        .then(result => {
+            res.send(result);
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+})
 
 app.get('/', (req, res) => res.send('Hola Mundo'));
